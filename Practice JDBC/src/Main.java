1afd02f8665dda245ba1150f6d25917e5c869577
FullMarksChaheye <k191069@nu.edu.pk>
2022-06-01 09:25:15 -0700

    JDBC Practice with SQL

--- Practice JDBC/src/Main.java
+++ Practice JDBC/src/Main.java
@@ -1,7 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Main {
 
-	public static void main(String[] args) {
+	static final String URL = "jdbc:mysql:///prac";
+	static final String USERNAME = "root";
+	static final String PASSWORD = "fast";
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		
+		if(con == null) {
+			throw new Exception("Couldn't Create the connection");
+		}
+		
+		PreparedStatement ps = con.prepareStatement("SHOW TABLES;");
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			System.out.println(rs.getString(1));
+		}
+		
+		rs.close();
+		ps.close();
+		con.close();
 		
 	}
 
